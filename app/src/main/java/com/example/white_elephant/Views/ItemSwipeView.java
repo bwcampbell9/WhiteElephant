a43db0cee7eec2147ff9b0c4e892a233d897cd03
Bryce <bwcampbell9@gmail.com>
2020-05-12 16:06:58 -0500

    Add main swipe page and improve card style.

--- app/src/main/java/com/example/white_elephant/Views/ItemSwipeView.java
+++ app/src/main/java/com/example/white_elephant/Views/ItemSwipeView.java
@@ -1,16 +1,33 @@
 package com.example.white_elephant.Views;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.white_elephant.Models.ItemModel;
 import com.example.white_elephant.R;
 
 public class ItemSwipeView extends Fragment implements BaseView {
+
+    int width, height;
+    double xCenter, yCenter;
+    int x_cord, y_cord, x, y;
+    static final double MAX_ROTATION = .2;
+
+    enum Status {
+        NOTHING,
+        LIKE,
+        DISLIKE,
+        SAVE,
+    }
+    Status status = Status.NOTHING;
 
     /**
      * Create a new instance of ItemSwipeView, initialized to
@@ -27,18 +44,116 @@ public class ItemSwipeView extends Fragment implements BaseView {
         return f;
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.item_swipe_layout, container, false);
-
+        final View view = inflater.inflate(R.layout.item_swipe_layout, container, false);
+        view.findViewById(R.id.imageView).setClipToOutline(true);
         TextView textView = (TextView) view.findViewById(R.id.titleText);
         TextView descriptionView = (TextView) view.findViewById(R.id.descriptionText);
+
+        final ImageView likeView = view.findViewById(R.id.likeIcon);
+        final ImageView dislikeView = view.findViewById(R.id.dislikeIcon);
+        final ImageView saveView = view.findViewById(R.id.saveIcon);
+
+        likeView.setAlpha(0f);
+        dislikeView.setAlpha(0f);
+        saveView.setAlpha(0f);
 
         ItemModel model = getModel();
 
         textView.setText(model.getName() + ", $" + model.getValue());
         descriptionView.setText(model.getDescription());
+
+        view.setX(0);
+        view.setY(0);
+        view.setRotation(0);
+        likeView.setAlpha(0f);
+        dislikeView.setAlpha(0f);
+        saveView.setAlpha(0f);
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                width = container.getWidth();
+                height = container.getHeight();
+                xCenter = width / 2;
+                yCenter = height / 2;
+
+                // TODO: handle possible crash here if this fragment is used outside of MainSwipeView
+                MainSwipeView parentActivity = (MainSwipeView) getActivity();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        x = (int) event.getRawX();
+                        y = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+
+                        x_cord = (int) event.getRawX() - x;
+                        // smoother animation.
+                        y_cord = (int) event.getRawY() - y;
+
+                        view.setX(x_cord);
+                        view.setY(y_cord);
+                        view.setRotation((float) ((x_cord / xCenter) * (90) * MAX_ROTATION));
+                        float xVal = (float)((x_cord) / (xCenter / 2.0));
+                        float yVal = (float)((y_cord) / (yCenter / 2.0));
+                        if(xVal < .5 && xVal > -.5) {
+                            saveView.setAlpha(-yVal);
+                        } else {
+                            saveView.setAlpha(0f);
+                        }
+                        likeView.setAlpha(xVal);
+                        dislikeView.setAlpha(-xVal);
+
+                        if (x_cord > (xCenter / 2)) {
+                            status = Status.LIKE;
+                        } else if (x_cord < -(xCenter / 2.0)) {
+                            status = Status.DISLIKE;
+                        } else if (y_cord < -(yCenter / 2.0)) {
+                            status = Status.SAVE;
+                        } else {
+                            status = Status.NOTHING;
+                        }
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        x_cord = (int) event.getRawX() - x;
+                        y_cord = (int) event.getRawY() - y;
+
+                        //tvUnLike.setAlpha(0);
+                        //tvLike.setAlpha(0);
+
+                        switch (status) {
+                            case LIKE:
+                                parentActivity.likeItem();
+                                break;
+                            case DISLIKE:
+                                parentActivity.dislikeItem();
+                                break;
+                            case SAVE:
+                                parentActivity.saveItem();
+                                break;
+                            case NOTHING:
+                                view.setX(0);
+                                view.setY(0);
+                                view.setRotation(0);
+                                likeView.setAlpha(0f);
+                                dislikeView.setAlpha(0f);
+                                saveView.setAlpha(0f);
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/example/white_elephant/Views/MainSwipeView.java
@@ -0,0 +1,86 @@
+package com.example.white_elephant.Views;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.RelativeLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.white_elephant.Models.ItemModel;
+
+import java.util.ArrayList;
+
+import com.example.white_elephant.R;
+
+public class MainSwipeView extends AppCompatActivity {
+
+    public RelativeLayout parentView;
+    private FragmentManager fragMan;
+    private Context context;
+
+    ArrayList<ItemSwipeView> itemViewList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main_swipe_layout);
+
+        fragMan = getFragmentManager();
+
+        context = MainSwipeView.this;
+
+        parentView = (RelativeLayout) findViewById(R.id.swipe_cards_layout);
+
+        itemViewList = new ArrayList<>();
+
+        getArrayData();
+
+        FragmentTransaction fragTransaction = fragMan.beginTransaction();
+        if (itemViewList.size() > 1) {
+            fragTransaction.add(R.id.swipe_cards_layout, itemViewList.get(1));
+        }
+        if (itemViewList.size() > 0) {
+            fragTransaction.add(R.id.swipe_cards_layout, itemViewList.get(0));
+        }
+        fragTransaction.commit();
+
+
+    }
+
+    private ItemSwipeView popTopItem() {
+        if(itemViewList.size() == 0) {
+            return null;
+        }
+        FragmentTransaction fragTransaction = fragMan.beginTransaction();
+        fragTransaction.remove(itemViewList.get(0));
+        if(itemViewList.size() > 2) {
+            fragTransaction.detach(itemViewList.get(1));
+            fragTransaction.add(R.id.swipe_cards_layout, itemViewList.get(2));
+            fragTransaction.attach(itemViewList.get(1));
+        }
+        fragTransaction.commit();
+
+        return itemViewList.remove(0);
+    }
+
+    public void likeItem() {
+        popTopItem();
+    }
+
+    public void dislikeItem() {
+        popTopItem();
+    }
+    public void saveItem() {
+        popTopItem();
+    }
+
+    private void getArrayData() {
+        for(int i = 0; i < 5; i++) {
+            ItemModel model = new ItemModel("Neat Shoes", "These are my super neat shoes", i * 10);
+            ItemSwipeView view = ItemSwipeView.newInstance(model);
+            itemViewList.add(view);
+        }
+    }
+}
