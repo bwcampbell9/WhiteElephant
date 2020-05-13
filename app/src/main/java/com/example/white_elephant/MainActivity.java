ef0b9c357031e62ce81c3baf801c8b60b1560346
Julian <jdavi104@calpoly.edu>
2020-05-13 00:08:28 -0700

    Added Post Item Functionality

--- app/src/main/java/com/example/white_elephant/MainActivity.java
+++ app/src/main/java/com/example/white_elephant/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.white_elephant;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_swipe_layout);
+
+        Toast.makeText(MainActivity.this, "Firebase Connection Succes", Toast.LENGTH_LONG).show();
     }
 }
