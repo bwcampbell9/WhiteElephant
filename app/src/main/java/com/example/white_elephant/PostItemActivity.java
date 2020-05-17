60d731f58e4647143eaec7d839e98a85009358b6
Julian Davis <juliandavis715@gmail.com>
2020-05-17 11:27:02 -0700

    Upload image functionality

--- app/src/main/java/com/example/white_elephant/PostItemActivity.java
+++ app/src/main/java/com/example/white_elephant/PostItemActivity.java
@@ -1,34 +1,90 @@
 package com.example.white_elephant;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.white_elephant.Models.ItemModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class PostItemActivity extends AppCompatActivity {
+
+    public boolean uploaded = false;
+    public boolean chose = false;
 
     EditText nameEditText;
     EditText descEditText;
     EditText valEditText;
-    EditText postErrorEditText;
+
+    Button chooseBtn, uploadBtn;
+    ImageView imageView;
+    StorageReference mStorageRef;
+    public Uri imageUri;
 
     Button addItemBtn;
     DatabaseReference reff;
     ItemModel item;
+    private StorageTask uploadTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_item);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+
+        chooseBtn = (Button) findViewById(R.id.chooseBtn);
+        uploadBtn = (Button) findViewById(R.id.uploadBtn);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        chooseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploaded = false;
+                chose = true;
+                myFileChooser();
+            }
+        });
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (uploadTask != null && uploadTask.isInProgress()){
+                    makeToast("Upload in progress");
+                } else if (uploaded == true){
+                    makeToast("Item already uploaded");
+                } else if (chose == false){
+                    makeToast("Choose an image first");
+                } else{
+                    System.out.println("here");
+                    myFileUploader();
+                }
+
+            }
+        });
+
         nameEditText = (EditText) findViewById(R.id.nameEditText);
         descEditText = (EditText) findViewById(R.id.descEditText);
         valEditText = (EditText) findViewById(R.id.valEditText);
-        postErrorEditText = (EditText) findViewById(R.id.postErrorEditText);
         item = new ItemModel();
         addItemBtn = (Button) findViewById(R.id.addItemBtn);
 
@@ -49,17 +105,75 @@ public class PostItemActivity extends AppCompatActivity {
                     val = Double.parseDouble(valEditText.getText().toString().trim());
                     item.setValue(val);
                     if (name.length() <= 0 || val < 0){
-                        postErrorEditText.setText("Item Not Added: Invalid Input");
+                        makeToast("Item Not Added, Try Again");
                     } else{
                         reff.push().setValue(item);
-                        Toast.makeText(PostItemActivity.this, "Item Added Successfully", Toast.LENGTH_LONG).show();
+                        makeToast("Item Added Successfully");
                     }
                 } catch (Exception e){
-                    postErrorEditText.setText("Item Not Added: Invalid Input");
+                    makeToast("Item Not Added, Try Again");
                 }
 
             }
         });
 
     }
+
+    private void myFileChooser () {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+
+    }
+
+    private void myFileUploader(){
+        StorageReference reff = mStorageRef.child(System.currentTimeMillis() + "." + getExtension(imageUri));
+        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+
+        uploadTask = reff.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl()
+                        Toast.makeText(PostItemActivity.this,
+                                "Image Uploaded Successfully", Toast.LENGTH_LONG).show();
+                        uploaded = true;
+                    }
+
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+
+
+    }
+
+    private String getExtension(Uri mUri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(mUri));
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK &&
+                data != null && data.getData() != null){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+    private void makeToast(String str){
+        Toast.makeText(PostItemActivity.this, str, Toast.LENGTH_LONG).show();
+    }
+
 }
