d809d61fc901b7aaa32c6c8dca9b0839a3d72772
daxwa <dwagne02@calpoly.edu>
2020-05-25 18:17:10 -0700

    Close Itemview in closet!

--- app/src/main/java/com/example/white_elephant/MyClosetFragment.java
+++ app/src/main/java/com/example/white_elephant/MyClosetFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +21,7 @@ import java.util.LinkedList;
 public class MyClosetFragment extends Fragment implements ClosetAdapter.ItemClickListener {
 
     ClosetAdapter adapter;
+    private Button goBack;
 
     public MyClosetFragment() {
         // Required empty public constructor
@@ -64,4 +67,5 @@ public class MyClosetFragment extends Fragment implements ClosetAdapter.ItemClic
         ft.replace(R.id.Closet, fragment);
         ft.commit();
     }
+
 }
--- app/src/main/java/com/example/white_elephant/ViewMyItem.java
+++ app/src/main/java/com/example/white_elephant/ViewMyItem.java
@@ -3,10 +3,13 @@ package com.example.white_elephant;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.white_elephant.models.ItemModel;
@@ -15,7 +18,9 @@ import com.example.white_elephant.models.ItemModel;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ViewMyItem extends Fragment {
+public class ViewMyItem extends Fragment implements View.OnClickListener
+ {
+     private Button goBack;
 
     public ViewMyItem() {
         // Required empty public constructor
@@ -34,10 +39,21 @@ public class ViewMyItem extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         ItemModel item = (ItemModel)getArguments().getSerializable("a");
         View view = inflater.inflate(R.layout.fragment_view_my_item, container, false);
         ((TextView)view.findViewById(R.id.Title)).setText(item.getName());
         ((TextView)view.findViewById(R.id.Details)).setText(item.getDescription());
+        goBack = view.findViewById(R.id.buttonClose);
+        goBack.setOnClickListener(this);
         return view;
+    }
+
+    public void onClick(View v){
+        //getActivity().getFragmentManager().beginTransaction().remove(this).commit();
+        ((MainActivity)getActivity()).setState(0);
+                NavController navController = ((MainActivity)getActivity()).getNavController();
+                NavDirections action = MyClosetFragmentDirections.actionMyClosetFragmentSelf();
+                navController.navigate(action);
     }
 }
