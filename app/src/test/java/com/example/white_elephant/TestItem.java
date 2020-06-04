d9cd25b2e7bfd1da4162617e3f84d821d494bf32
Bryce <bwcampbell9@gmail.com>
2020-06-04 18:49:07 -0500

    Add TestItem suite.

---
+++ app/src/test/java/com/example/white_elephant/TestItem.java
@@ -0,0 +1,50 @@
+package com.example.white_elephant;
+
+import com.example.white_elephant.models.Item;
+import com.example.white_elephant.util.Database;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * unit test will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class TestItem {
+    @Test
+    public void getName() {
+        Item item = new Item("test1", "desc", 12.5, new ArrayList<String>());
+
+        assertEquals("test1", item.getName());
+    }
+
+    @Test
+    public void getValue() {
+        Item item = new Item("test1", "desc", 12.5, new ArrayList<String>());
+
+        assertEquals(12.5, item.getValue(), .0001);
+    }
+    @Test
+    public void getDescription() {
+        Item item = new Item("test1", "desc", 12.5, new ArrayList<String>());
+
+        assertEquals("desc", item.getDescription());
+    }
+    @Test
+    public void setValue() {
+        Item item = new Item("test1", "desc", 12.5, new ArrayList<String>());
+        item.setValue(1.23);
+        assertEquals(1.23, item.getValue(), .0001);
+    }
+    @Test
+    public void setName() {
+        Item item = new Item("test1", "desc", 12.5, new ArrayList<String>());
+        item.setName("new_name");
+        assertEquals("new_name", item.getName());
+    }
+
+}
